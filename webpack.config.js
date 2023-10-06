e8949da1f94c04ed40e9e2eed181bdc16bdb2eec
erikpastuszek@gmail.com <erikpastuszek@gmail.com>
2023-10-05 21:30:16 -0300

    Added base of the project

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      extensions: ['.js', '.jsx'],
+    },
+  },
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
